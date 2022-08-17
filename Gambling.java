dd261f19a917f762819690b7a92e266df13a80de
Sanjithj <sanjithj591997@gmail.com>
2022-08-17 15:38:38 +0530

    Added: Daily bet

--- Gambling.java
+++ Gambling.java
@@ -2,8 +2,14 @@ package com.bridgelabz;
 
 public class Gambling {
 	
+	public static int stake = 100;
+	public static int bet = 1;
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Gambling Program");
+		
+		System.out.println("Stake" +stake+ "$");
+		System.out.println("bet" +bet+ "$");
 	}
 
 }
